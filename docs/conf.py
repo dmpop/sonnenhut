43b2667e7e2bd5fd4d7d2f0c407de1ac4958d045
Dmitri Popov <dpopov@suse.de>
2017-02-21 14:05:43 +0100

    Fixed #16 (added manapage)

---
+++ docs/conf.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+import os
+
+
+extensions = [
+    'sphinx.ext.todo',
+]
+if os.getenv('SPELLCHECK'):
+    extensions += 'sphinxcontrib.spelling',
+    spelling_show_suggestions = True
+    spelling_lang = 'en_US'
+
+source_suffix = '.rst'
+master_doc = 'sonnenhut'
+project = 'sonnenhut'
+year = '2017'
+author = 'Dmitri Popov'
+copyright = '{0}, {1}'.format(year, author)
+version = release = '1.0.1'
+
+pygments_style = 'trac'
+templates_path = ['.']
+extlinks = {
+    'issue': ('https://github.com/dmpop/sonnenhut/issues/%s', '#'),
+    'pr': ('https://github.com/dmpop/sonnenhut/pull/%s', 'PR #'),
+}
+# on_rtd is whether we are on readthedocs.org
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+
+if not on_rtd:  # only set the theme if we're building docs locally
+    html_theme = 'sphinx_rtd_theme'
---
+++ docs/sonnenhut.rst
@@ -0,0 +1,26 @@
+.. sonnenhut documentation master file for manpage
+
+:orphan:
+
+sonnenhut |version| -- Manual Page
+==================================
+A simple tool for photographers
+
+Synopsis
+--------
+::
+   sonnenhut CITYNAME
+
+Description
+-----------
+A tool to run before a photo walk
+
+Examples
+--------
+::
+   sonnenhut tokyo
+
+Author
+------
+
+Dmitri Popov, Thomas Schraitle
